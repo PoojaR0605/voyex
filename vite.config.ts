486c28ec05be08789682441419e049b5832dedda
Pooja Rajappa <poojarajappa06@gmail.com>
2026-04-13 20:50:06 +0530

    Fix vercel routing

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/voyex/',   
+
+  // ❗ IMPORTANT FIX (this was causing white screen)
+  base: '/',   // ✅ CHANGE THIS (NOT /voyex/)
 });
